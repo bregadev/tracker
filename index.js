cdcdd7f4b0cedcb7c580562a9ebd8d67429b52e3
Horacio Diaz <bregadev@gmail.com>
2020-09-05 22:42:26 -0500

    update master

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const game_model = require('./game_model');
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get('/', (req, res) => {
+  game_model.getGames()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+// app.post('/game', (req, res) => {
+//   game_model.createGame(req.body)
+//   .then(response => {
+//     res.status(200).send(response);
+//   })
+//   .catch(error => {
+//     res.status(500).send(error);
+//   })
+// })
+
+// app.delete('/games/:id', (req, res) => {
+//   game_model.deletegame(req.params.id)
+//   .then(response => {
+//     res.status(200).send(response);
+//   })
+//   .catch(error => {
+//     res.status(500).send(error);
+//   })
+// })
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
---
+++ react-tracker/src/columns.js
@@ -0,0 +1,33 @@
+ const columns = [
+    {
+      Header: 'Id',
+      accessor: 'id',
+    },
+    {
+      Header: 'winner',
+      accessor: 'winner',
+    },
+    {
+      Header: 'Loser',
+      accessor: 'loser',
+    },
+    {
+      Header: 'Winning Score',
+      accessor: 'winning_score',
+    },
+    {
+      Header: 'Losing Score',
+      accessor: 'losing_score',
+    },
+    {
+        Header: 'Game Date',
+        accessor: 'game_date',
+      },
+      {
+        Header: 'Dub',
+        accessor: 'dub',
+      },
+    
+  ];
+
+  export default columns;
